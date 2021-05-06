63185efa3866e77d363db1cc678eaafba5cb3c6d
yusuke-abe <yusuke.abe@force-corp.co.jp>
2021-05-06 10:10:08 +0900

    restoration

---
+++ js/jspractice.js
@@ -0,0 +1,42 @@
+// document.write("Hello World!");
+// alert("Hwllo World");
+
+// console.log("Hello World" + "田中さん");
+// console.log(10 +10);
+// console.log(" 10 + 10 ");
+
+// console.log(1 + 1);
+// console.log(1 - 1);
+// console.log(2 * 2);
+// console.log(10 / 3);
+// console.log(10 % 3);
+
+// let x =1;
+// let y =1;
+// x++;
+// console.log(x);
+// y--;
+// console.log(y);
+
+// let x = 7;
+// let y = x++;
+// let x2 = 7;
+// let y2 = ++x2;
+
+
+// console.log(x);
+// console.log(y);
+// console.log(x2);
+// console.log(y2);
+
+'use strict';
+
+let age = 30;
+const name = "田中太郎";
+
+age = age + 2;
+
+console.log(`${name}さんの年齢は${age}です`);
+
+// var 変数名/定数　←使わない
+// var = let  const
